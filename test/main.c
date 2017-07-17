1ea025d18f424f1e1156ba70ca2114223811c326
hluffy <18801613171@163.com>
2017-07-18 00:00:54 +0800

    first commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Rikka on 2017/7/17.
+//  Copyright © 2017年 Rikka. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
